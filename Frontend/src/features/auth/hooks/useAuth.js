6911eeac7c15af102d2e4b87ddaf418b2ad853a0
Namit Joshi <joshinamit64@gmail.com>
2026-06-19 17:02:19 +0530

    fixed some issues

--- Frontend/src/features/auth/hooks/useAuth.js
+++ Frontend/src/features/auth/hooks/useAuth.js
@@ -2,21 +2,19 @@ import { useContext, useEffect } from "react";
 import { AuthContext } from "../auth.context";
 import { login, register, logout, getMe } from "../services/auth.api";
 
-
-
 export const useAuth = () => {
 
     const context = useContext(AuthContext)
     const { user, setUser, loading, setLoading } = context
 
-
     const handleLogin = async ({ email, password }) => {
         setLoading(true)
         try {
             const data = await login({ email, password })
+            localStorage.setItem("token", data.token) // ✅ token save karo
             setUser(data.user)
         } catch (err) {
-
+            console.log(err)
         } finally {
             setLoading(false)
         }
@@ -26,9 +24,10 @@ export const useAuth = () => {
         setLoading(true)
         try {
             const data = await register({ username, email, password })
+            localStorage.setItem("token", data.token) // ✅ token save karo
             setUser(data.user)
         } catch (err) {
-
+            console.log(err)
         } finally {
             setLoading(false)
         }
@@ -37,29 +36,29 @@ export const useAuth = () => {
     const handleLogout = async () => {
         setLoading(true)
         try {
-            const data = await logout()
+            await logout()
+            localStorage.removeItem("token") // ✅ token hata do
             setUser(null)
         } catch (err) {
-
+            console.log(err)
         } finally {
             setLoading(false)
         }
     }
 
     useEffect(() => {
-
         const getAndSetUser = async () => {
             try {
-
                 const data = await getMe()
                 setUser(data.user)
-            } catch (err) { } finally {
+            } catch (err) { 
+                console.log(err)
+            } finally {
                 setLoading(false)
             }
         }
 
         getAndSetUser()
-
     }, [])
 
     return { user, loading, handleRegister, handleLogin, handleLogout }
