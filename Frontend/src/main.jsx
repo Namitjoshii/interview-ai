08e9a6df6526d1c9c262ecfa9bd02159c0a34a4f
Namit Joshi <joshinamit64@gmail.com>
2026-06-19 18:55:53 +0530

    added postHog services

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,6 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import "./style.scss"
+import posthog from 'posthog-js'
+
+posthog.init(
+  'phc_wEJWzKnEFc8T5qdLWGfD5uHotzigiX6yQZ2xkEG3Tu2b',
+  {
+    api_host: 'https://us.i.posthog.com',
+    person_profiles: 'identified_only'
+  }
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
